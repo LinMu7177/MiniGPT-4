18d0bf5f80455e3004e989bfe8c4f0b49fdc70a2
Alan <annanli035@gmail.com>
2023-09-16 11:09:44 +0800

    adapt to vqa

--- minigpt4/models/modeling_llama.py
+++ minigpt4/models/modeling_llama.py
@@ -425,7 +425,9 @@ class LlamaModel(LlamaPreTrainedModel):
         self.vocab_size = config.vocab_size
 
         self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
-        self.layers = nn.ModuleList([LlamaDecoderLayer(config) for _ in range(config.num_hidden_layers)])
+        # just load one layer
+        self.layers = nn.ModuleList([LlamaDecoderLayer(config) for _ in range(1)])
+        # self.layers = nn.ModuleList([LlamaDecoderLayer(config) for _ in range(config.num_hidden_layers)])
         self.norm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
 
         self.gradient_checkpointing = False
